e4aa422e03387daa0e37628294e0aa6ea13f8f69
kurbanov-10 <dilshodqurbonov920@gmail.com>
2026-04-01 14:26:45 +0500

    added homepage, uvicorn

--- main.py
+++ main.py
@@ -4,18 +4,22 @@
 # app = FastAPI()
 
 # app.include_router(api_router)
-
 from fastapi import FastAPI
 from api import api_router
 from fastapi.middleware.cors import CORSMiddleware
-
-
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.get("/", response_class=HTMLResponse)
+def home():
+    return "<h1>Blog App ishlayapti 🚀</h1>"
+
 app.include_router(api_router)
